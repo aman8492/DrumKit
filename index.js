b92fefadfe0851773ecbb33d1678f5379cfed72e
Aman <97701226+aman8492@users.noreply.github.com>
2022-09-17 15:44:12 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,44 +1,58 @@
-var length=document.querySelectorAll("button").length;
-var gaana=new Audio("Gaddi Vich.mp3");
-function funct(){
-    var btn=this.innerHTML;
-    switch (btn) {
-    case "a":
-        var audio=new Audio("03.mp3");
-        audio.play();
-        break;
-    case "b":
-        var audio=new Audio("02.mp3");
-        audio.play();
-        break;
-    case "c":
-            var audio=new Audio("03.mp3");
-            audio.play();
-            break;
-    case "d":
-            var audio=new Audio("02.mp3");
-            audio.play();
-            break;
-    case "e":
-            var audio=new Audio("02.mp3");
-            audio.play();
-            break;
-     case "f":
-            var audio=new Audio("03.mp3");
-            audio.play();
-            break;
-    case "g":
-            var audio=new Audio("02.mp3");
-            audio.play();
-            break;}
+var length = document.querySelectorAll("button").length;
+for (let i = 0; i < length; i++) {
+    document.querySelectorAll("button")[i].addEventListener("click", function () {
+        var keyy = this.innerHTML;
+        makesound(keyy)
+    });
 }
 
 
 
+document.addEventListener("keypress", function (event) {
 
 
-for (var i=0;i<length;i++){
-    document.querySelectorAll("button")[i].addEventListener("click",funct);
-   
+
+    makesound(event.key);
+});
+
+
+function makesound(key) {
+    switch (key) {
+        case "a":
+            var audio = new Audio("02.mp3");
+            audio.play();
+            break;
+        case "b":
+            var audio = new Audio("02.mp3");
+            audio.play();
+            break;
+
+        case "c":
+            var audio = new Audio("02.mp3");
+            audio.play();
+            break;
+
+        case "d":
+            var audio = new Audio("02.mp3");
+            audio.play();
+            break;
+
+        case "e":
+            var audio = new Audio("02.mp3");
+            audio.play();
+            break;
+
+        case "f":
+            var audio = new Audio("02.mp3");
+            audio.play();
+            break;
+        case "g":
+            var audio = new Audio("02.mp3");
+            audio.play();
+            break;
+        default:
+            alert("Press the Key which are in there..");
+            break;
+    }
 
 }
